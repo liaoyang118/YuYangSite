d6d2dea57d52388b5d42c6a5d754b931952e0848
yang.liao <yang.liao@DESKTOP-KAK9LJ1>
2018-10-23 22:32:05 +0800

    延长超时时间

---
+++ Site.Log/LogHelp.cs
@@ -0,0 +1,34 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Site.Log
+{
+    public class LogHelp
+    {
+        private static ILog ErrorLogger;
+        private static ILog InfoLogger;
+        static LogHelp() 
+        {
+            ErrorLogger = LogManager.GetLogger("error");
+            InfoLogger = LogManager.GetLogger("info");
+        }
+
+        public static void Info(string content)
+        {
+            InfoLogger.Info(content);
+        }
+
+        public static void Error(string content)
+        {
+            ErrorLogger.Error(content);
+        }
+
+
+
+    }
+}
